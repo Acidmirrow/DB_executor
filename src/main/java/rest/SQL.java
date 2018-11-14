e3453f1176163bf77f46c2e9e6de60bf6812f286
Бобриков Борис Борисович <bbobrikov@at-consulting.ru>
2018-11-14 20:37:41 +0300

    my form

--- src/main/java/rest/SQL.java
+++ src/main/java/rest/SQL.java
@@ -16,51 +16,43 @@ public class SQL {
             "<html>\n" +
             "<head>\n" +
             "<base href=\"http://127.0.0.1:8081/SQL/\" target=\"_self\" />\n" +
-            "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
-            "<title>ВВЕДИТЕ SQL-ЗАПРОС</title>\n" +
+            "<meta http-equiv=\"Content-Type\"content=\"text/html; charset=UTF-8\">\n" +
             "</head>\n" +
             "<body>\n" +
-            "<form action=\"SQL_rs\" method=\"POST\">\n" +
-            "<fieldset align=\"center\">\n" +
-            "<legend><strong>ВВЕДИТЕ SQL-ЗАПРОС</strong></legend>\n" +
-            "<strong>SQL-запрос:</strong><input type=\"text\" name=\"sql\" size=\"40\" autofocus>\n" +
-            "<input type=\"submit\" value=\"Отправить\">\n" +
-            "</fieldset>\n" +
-            "</form>\n" +
+            "<form action=\"response\" method=\"POST\">\n" +
+            "<table width=\"300\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"1\">\n" +
+            "   <tr>\n" +
+            "      <td>\n" +
+            "         <form name=\"form1\" method=\"post\" action=\"insert_ac.php\">\n" +
+            "         <table width=\"100%\" border=\"0\" cellspacing=\"1\" cellpadding=\"3\">\n" +
+            "            <tr>\n" +
+            "               <td colspan=\"3\">Запрос в базу </td>\n" +
+            "            </tr>\n" +
+            "            <tr>\n" +
+            "               <td width=\"71\">SQL-скрипт</td>\n" +
+            "               <td width=\"6\">:</td>\n" +
+            "               <td width=\"301\"><input type=\"text\" name=\"sql\" size=\"40\" autofocus>\n" +
+            "            </tr>\n" +
+            "            <tr>\n" +
+            "               <td colspan=\"3\" align=\"center\"><input type=\"submit\" name=\"Отправить\" value=\"Отправить\"></td>\n" +
+            "            </tr>\n" +
+            "         </table>\n" +
+            "</form>"+
             "</body>\n" +
             "</html>";
-    /*private String form = "<meta http-equiv=\"Content-Type\"content=\"text/html; charset=UTF-8\">\n" +
-          "<table width=\"300\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"1\">\n" +
-          "   <tr>\n" +
-          "      <td>\n" +
-          "         <form name=\"form1\" method=\"post\" action=\"insert_ac.php\">\n" +
-          "         <table width=\"100%\" border=\"0\" cellspacing=\"1\" cellpadding=\"3\">\n" +
-          "            <tr>\n" +
-          "               <td colspan=\"3\">Запрос в базу </td>\n" +
-          "            </tr>\n" +
-          "            <tr>\n" +
-          "               <td width=\"71\">SQL-скрипт</td>\n" +
-          "               <td width=\"6\">:</td>\n" +
-          "               <td width=\"301\"><input type=\"text\" name=\"sql\" size=\"40\" autofocus>\n" +
-          "            </tr>\n" +
-          "            <tr>\n" +
-          "               <td colspan=\"3\" align=\"center\"><input type=\"submit\" name=\"Отправить\" value=\"Отправить\"></td>\n" +
-          "            </tr>\n" +
-          "         </table>\n" +
-          "         </form>";*/
 
-    //http://127.0.0.1:8081/SQL/SQL_rq
+    //http://127.0.0.1:8081/SQL/request
     @GET
-    @Path("SQL_rq")
+    @Path("request")
     @Produces(MediaType.TEXT_HTML)
     public String rqSQL() {
 
         return form;
     }
 
-    //http://127.0.0.1:8081/SQL/SQL_rs
+    //http://127.0.0.1:8081/SQL/response
     @POST
-    @Path("SQL_rs")
+    @Path("response")
     @Produces(MediaType.APPLICATION_JSON)
     public ArrayList<Sales> rsSQL(@FormParam("sql") String sql) throws SQLException, ClassNotFoundException {
 
